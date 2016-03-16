85d1c45ca4ac4d3266a50a04a4b51dca28f5d0f6
Vladimir Piskunov <vladimir.piskunov@gmail.com>
2016-03-17 01:49:09 +0300

    boolean exp adapter & sesion parameters fixes

---
+++ src/main/java/com/piskunov/xmlconverter/SessionParameters.java
@@ -0,0 +1,34 @@
+package com.piskunov.xmlconverter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Vladimir Piskunov on 2/29/16.
+ */
+public class SessionParameters {
+
+    public Map<String, String> getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(Map<String, String> parameters) {
+
+        for(Map.Entry<String, String> entry: parameters.entrySet()){
+            String value = System.getProperty(entry.getKey());
+            if(value != null){
+                entry.setValue(value);
+            }
+        }
+        this.parameters = parameters;
+    }
+
+    public String getProperty(String property){
+        return parameters.get(property);
+    }
+
+    private Map<String, String> parameters;
+
+
+
+}
