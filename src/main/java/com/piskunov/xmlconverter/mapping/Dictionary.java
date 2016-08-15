a2d3d8b69f2220ea6e2343d28fd2a62fb333e928
Boris Tveritnev <boris.tveritnev@xing.com>
2016-08-16 00:24:02 +0200

    mapping and fixes to map and convert partners feeds

--- src/main/java/com/piskunov/xmlconverter/mapping/Dictionary.java
+++ src/main/java/com/piskunov/xmlconverter/mapping/Dictionary.java
@@ -1,15 +1,12 @@
 package com.piskunov.xmlconverter.mapping;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.*;
-import java.util.logging.Logger;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.annotation.PostConstruct;
 
@@ -20,71 +17,77 @@ import org.springframework.core.io.Resource;
  */
 public class Dictionary {
 
-    private Map<String, List<String>> dictionary = new HashMap<>();
+	private Map<String, List<String>> dictionary = new HashMap<>();
+	private Map<String, Pattern> keyPatterns = new HashMap<>();
 
-    private Resource dictionaryFile;
+	private Resource dictionaryFile;
 
-    public Resource getDictionaryFile() {
-        return dictionaryFile;
-    }
+	public Resource getDictionaryFile() {
+		return dictionaryFile;
+	}
 
-    public void setDictionaryFile(Resource dictionaryFile) {
-        this.dictionaryFile = dictionaryFile;
-    }
+	public void setDictionaryFile(Resource dictionaryFile) {
+		this.dictionaryFile = dictionaryFile;
+	}
 
-    @PostConstruct
-    public void init() throws Exception {
+	@PostConstruct
+	public void init() throws Exception {
 
-        Scanner scanner = new Scanner(dictionaryFile.getFile());
-     //   scanner.useDelimiter("\n");
+		Scanner scanner = new Scanner(dictionaryFile.getFile());
+		// scanner.useDelimiter("\n");
 
-        while (scanner.hasNextLine()) {
+		while (scanner.hasNextLine()) {
 
-            Scanner lineScanner  = new Scanner(scanner.nextLine());
-            lineScanner.useDelimiter(",");
+			Scanner lineScanner = new Scanner(scanner.nextLine());
+			lineScanner.useDelimiter(",");
 
-            String key = null;
-            ArrayList<String> values = new ArrayList<>();
+			String key = null;
+			ArrayList<String> values = new ArrayList<>();
 
-            if(lineScanner.hasNext())
-                key = lineScanner.next();
+			if (lineScanner.hasNext())
+				key = lineScanner.next();
 
-            while (lineScanner.hasNext()){
-                values.add(lineScanner.next());
-            }
+			while (lineScanner.hasNext()) {
+				values.add(lineScanner.next());
+			}
 
-            if(key == null || values.size() == 0)
-                continue;
+			if (key == null || values.size() == 0)
+				continue;
 
-            List<String> currentValues = dictionary.get(key);
+			List<String> currentValues = dictionary.get(key);
 
-            if(currentValues != null){
-                currentValues.addAll(values);
-                dictionary.put(key, currentValues);
-            } else {
-                dictionary.put(key, values);
-            }
-        }
+			if (currentValues != null) {
+				currentValues.addAll(values);
+				dictionary.put(key, currentValues);
+			} else {
+				dictionary.put(key, values);
+			}
+		}
 
-    }
+	}
 
-    public List<String> search(String key, boolean searchByRegExp) {
+	public List<String> search(String key, boolean searchByRegExp) {
 
-        ArrayList<String> ret = new ArrayList<>();
+		ArrayList<String> ret = new ArrayList<>();
 
-        if (key == null)
-            return ret;
+		if (key == null)
+			return ret;
 
-        if(!searchByRegExp) {
-            return dictionary.get(key);
-        }
+		if (!searchByRegExp) {
+			return dictionary.get(key);
+		}
 
-        for (String dictionaryKey : dictionary.keySet()) {
-            if (key.matches(dictionaryKey)) {
-                ret.addAll(dictionary.get(dictionaryKey));
-            }
-        }
-        return ret;
-    }
-
+		for (String dictionaryKey : dictionary.keySet()) {
+			Pattern pattern = keyPatterns.get(dictionaryKey);
+			if (pattern == null) {
+				pattern = Pattern.compile(dictionaryKey);
+				keyPatterns.put(dictionaryKey, pattern);
+			}
+			Matcher matcher = pattern.matcher(key);
+			if (matcher.find()) {
+				ret.addAll(dictionary.get(dictionaryKey));
+			}
+		}
+		return ret;
+	}
 }
