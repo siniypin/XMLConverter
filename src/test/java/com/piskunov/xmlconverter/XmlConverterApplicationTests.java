38b291fff45fa459f636baddb07c00b16d1cf629
Boris Tveritnev <boris.tveritnev@xing.com>
2016-10-04 16:55:08 +0200

    fixed tests and massiv mapping

--- src/test/java/com/piskunov/xmlconverter/XmlConverterApplicationTests.java
+++ src/test/java/com/piskunov/xmlconverter/XmlConverterApplicationTests.java
@@ -3,13 +3,14 @@ package com.piskunov.xmlconverter;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
+@IntegrationTest({ "env=sandbox" })
 public class XmlConverterApplicationTests {
 
 	@Autowired
